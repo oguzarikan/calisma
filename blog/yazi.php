c829bf592cc48bc63c6e7abdfb952e4a1fd75605
oguzarikan <oguzzarikan@gmail.com>
2017-02-27 13:54:12 +0300

    Title değişti

--- blog/yazi.php
+++ blog/yazi.php
@@ -4,7 +4,7 @@ include("baglanti.php");
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <title>Bootstrap Example</title>
+    <title>Yaziları göster</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
